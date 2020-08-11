19e54cc5a899cd1fb3532c03c25350210a69ff0f
choisohyun <sohyun4283@naver.com>
2020-08-11 21:00:21 +0900

    feat. theme 타입을 d.ts 파일로 정의
    [#8]

---
+++ client/src/style/style.d.ts
@@ -0,0 +1,70 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    dark: {
+      mainBackground: string;
+      title: string;
+      primaryText: string;
+      secondaryText: string;
+      disable: string;
+      border: string;
+      divider: string;
+      background: string;
+      tableHeader: string;
+      blue1: string;
+      blue2: string;
+      blue3: string;
+      green: string;
+      gray1: string;
+      gray2: string;
+      gray3: string;
+      gray4: string;
+      black: string;
+      red: string;
+      white: string;
+    };
+    light: {
+      mainBackground: string;
+      title: string;
+      primaryText: string;
+      secondaryText: string;
+      disable: string;
+      border: string;
+      divider: string;
+      background: string;
+      tableHeader: string;
+      blue1: string;
+      blue2: string;
+      blue3: string;
+      green: string;
+      gray1: string;
+      gray2: string;
+      gray3: string;
+      gray4: string;
+      black: string;
+      red: string;
+      white: string;
+    };
+    fontSizes: {
+      xsm: string;
+      sm: string;
+      md: string;
+      lg: string;
+      xl: string;
+      xxl: string;
+    };
+    fontWeights: {
+      extraBold: number;
+      bold: number;
+      semiBold: number;
+      regular: number;
+    };
+    media: {
+      desktop: string;
+      tablet: string;
+      phone: string;
+    };
+    response: {};
+  }
+}
