d713b12484da88ebb1b21febd93e212266eca958
choisohyun <sohyun4283@naver.com>
2020-08-11 22:10:51 +0900

    feat: 라우팅 처리 lazy 로딩 하도록 설정
    [#5]
    - 추후 SSR을 적용하게 된다면 다른 방법으로 변경 예정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,33 @@ import { Reset } from "styled-reset";
 
 import { theme } from "@Style/theme";
 
+const MainPage = lazy(() => import("@MainPage/MainPage"));
+const MyPage = lazy(() => import("@MyPage/MyPage"));
+const QuizListPage = lazy(() => import("@QuizListPage/QuizListPage"));
+const QuizDetailPage = lazy(() => import("@QuizDetailPage/QuizDetailPage"));
+const QuizPage = lazy(() => import("@QuizPage/QuizPage"));
+const NewQuizPage = lazy(() => import("@NewQuizPage/NewQuizPage"));
+const Loading = lazy(() => import("@Loading/Loading"));
+const ErrorPage = lazy(() => import("@ErrorPage/ErrorPage"));
+
 const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Reset />
+        <Router>
+          <Suspense fallback={Loading}>
+            <Switch>
+              <Route path="/" exact component={MainPage} />
+              <Route path="/mypage" exact component={MyPage} />
+              <Route path="/quiz" exact component={QuizListPage} />
+              <Route path="/quiz/:quizId" exact component={QuizDetailPage} />
+              <Route path="/quiz/:quizId/:quizNumber" exact component={QuizPage} />
+              <Route path="/new" exact component={NewQuizPage} />
+              <Route component={ErrorPage} />
+            </Switch>
+          </Suspense>
+        </Router>
       </ThemeProvider>
     </>
   );
