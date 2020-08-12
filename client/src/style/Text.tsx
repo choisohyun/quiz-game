bd13cb0bb4c33e24b842dbec8f282bb8a0dcc300
choisohyun <sohyun4283@naver.com>
2020-08-12 21:55:53 +0900

    fix. Text 스타일 컴포넌트가 props를 undefined도 받을 수 있도록 수정
    [#14]

--- client/src/style/Text.tsx
+++ client/src/style/Text.tsx
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 
 interface IText {
-  color: string;
-  fontSize: string;
-  fontWeight: string;
+  color?: string;
+  fontSize?: string;
+  fontWeight?: string;
   lineHeight?: string;
   isClick?: boolean;
 }
 
 const Text = styled.span<IText>`
   display: inline-block;
-  color: ${({ theme, color }) => theme.light[color] || theme.light.black};
-  font-size: ${({ theme, fontSize }) => theme.fontSizes[fontSize] || theme.fontSizes.md};
-  font-weight: ${({ theme, fontWeight }) => theme.fontWeights[fontWeight] || theme.fontWeights.regular};
+  color: ${({ theme, color }) => (color && theme.light[color]) || theme.light.black};
+  font-size: ${({ theme, fontSize }) => (fontSize && (theme.fontSizes[fontSize] || fontSize)) || theme.fontSizes.md};
+  font-weight: ${({ theme, fontWeight }) => (fontWeight && theme.fontWeights[fontWeight]) || theme.fontWeights.regular};
   line-height: ${({ lineHeight }) => lineHeight};
   cursor: ${({ isClick }) => (isClick ? "pointer" : "default")};
 `;
