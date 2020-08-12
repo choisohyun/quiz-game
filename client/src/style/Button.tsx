57cd8eea8a677153d6027c2276323d4453e8b589
choisohyun <sohyun4283@naver.com>
2020-08-12 18:50:13 +0900

    fix. Button 스타일 컴포넌트가 props를 undefined도 받을 수 있도록 수정
    [#11]

--- client/src/style/Button.tsx
+++ client/src/style/Button.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 interface IButton {
-  backgroundColor: string;
-  color: string;
-  fontSize: string;
-  borderColor: string;
+  backgroundColor?: string;
+  color?: string;
+  fontSize?: string;
+  borderColor?: string;
   bold?: string;
   paddingSize?: string;
   disabled?: boolean;
@@ -12,15 +12,16 @@ interface IButton {
 }
 
 const Button = styled.button.attrs({ type: "button" })<IButton>`
-  background-color: ${({ theme, backgroundColor }) => theme.light[backgroundColor] || theme.light.green};
-  color: ${({ theme, color }) => theme.light[color] || theme.light.white};
-  font-size: ${({ theme, fontSize }) => theme.fontSizes[fontSize] || theme.fontSizes.md};
+  background-color: ${({ theme, backgroundColor }) =>
+    (backgroundColor && theme.light[backgroundColor]) || theme.light.green};
+  color: ${({ theme, color }) => (color && theme.light[color]) || theme.light.white};
+  font-size: ${({ theme, fontSize }) => (fontSize && theme.fontSizes[fontSize]) || theme.fontSizes.md};
   font-weight: ${({ bold }) => (bold ? "bold" : "normal")};
   pointer-events: ${({ disabled }) => (disabled ? "none" : "auto")};
   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
   opacity: ${({ disabled }) => (disabled ? 0.3 : 1)};
-  border-radius: 5px;
-  border: 1px solid ${({ theme, borderColor }) => theme.light[borderColor] || theme.light.gray2};
+  border-radius: 10px;
+  border: 1px solid ${({ theme, borderColor }) => (borderColor && theme.light[borderColor]) || theme.light.gray2};
   text-align: center;
   padding: ${({ paddingSize }) => (paddingSize ? paddingSize : "10px 15px")};
   display: flex;
