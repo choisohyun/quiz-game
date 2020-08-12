2bb08fe213a93db1dae9d09c3ed24f56c3089191
choisohyun <sohyun4283@naver.com>
2020-08-12 18:54:17 +0900

    feat. 로그인 여부에 따라 Header 다른 재사용 컴포넌트 생성
    [#11]
    - 기존 기획서에 있던 "퀴즈 만들기" 버튼은 다른 부분에 추가할 예정임

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,120 @@
 import React from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
 
-const Header = () => {
-  return <div></div>;
+import Text from "@Style/Text";
+import Button from "@Style/Button";
+
+interface IHeader {
+  type: string;
+}
+
+const Header = ({ type }: IHeader) => {
+  let history = useHistory();
+
+  const onPassMain = () => history.push("/");
+
+  return (
+    <>
+      <Wrapper>
+        <LeftWrapper>
+          <Text color="white" fontSize="xxl" fontWeight="extraBold" isClick onClick={onPassMain}>
+            QUIZY
+          </Text>
+          <SearchBar>
+            <SearchInputIcon />
+            <SearchInput placeholder="Search" />
+          </SearchBar>
+        </LeftWrapper>
+        <RightWrapper>
+          {type === "none" ? (
+            <UserButton>Login</UserButton>
+          ) : (
+            <LogoutWrapper>
+              <UserButton>Logout</UserButton>
+              <Avatar src="" />
+            </LogoutWrapper>
+          )}
+        </RightWrapper>
+      </Wrapper>
+    </>
+  );
 };
+
+const Wrapper = styled.header`
+  width: 100%;
+  height: 100px;
+  background-color: ${({ theme }) => theme.light.green};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 40px;
+  position: relative;
+`;
+
+const LeftWrapper = styled.div`
+  width: 50%;
+  justify-content: center;
+  align-items: center;
+  margin-left: 30px;
+  ${media.tablet} {
+    display: flex;
+    width: 50%;
+  }
+`;
+
+const RightWrapper = styled.div`
+  margin-right: 30px;
+`;
+
+const SearchBar = styled.form`
+  position: relative;
+  height: 100%;
+  width: 100%;
+`;
+
+const SearchInput = styled.input`
+  padding-left: 30px;
+  background: none;
+  color: ${({ theme }) => theme.light.white};
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.light.white};
+  outline: none;
+  height: 30px;
+  width: inherit;
+  font-size: 16px;
+  &:focus {
+    outline: none;
+    border-color: ${({ theme }) => theme.light.blue1};
+    box-shadow: inset 0 1px 2px ${({ theme }) => theme.light.blue3}, 0 0 0 0.2em ${({ theme }) => theme.light.blue2};
+  }
+`;
+
+const SearchInputIcon = styled(SearchIcon)`
+  display: block;
+  color: ${({ theme }) => theme.light.white};
+  text-align: center;
+  pointer-events: none;
+  position: absolute;
+  top: 6px;
+  left: 8px;
+`;
+
+const UserButton = styled(Button)`
+  height: 34px;
+`;
+
+const LogoutWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Avatar = styled.img`
+  height: 30px;
+  width: 30px;
+  border-radius: 5px;
+  margin: 0 15px 0 10px;
+`;
 
 export default Header;
