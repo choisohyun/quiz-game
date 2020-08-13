8c7ffaa6ebe2ca7eb6f3d74c7740b95f847113e6
choisohyun <sohyun4283@naver.com>
2020-08-13 14:58:16 +0900

    refactor. 사용자 프로필 사진 스타일 컴포넌트를 재사용 스타일로 분리
    [#14]

---
+++ client/src/style/Avatar.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Avatar = styled.img`
+  height: 30px;
+  width: 30px;
+  border-radius: 5px;
+  margin: 0 15px 0 10px;
+`;
+
+export default Avatar;
