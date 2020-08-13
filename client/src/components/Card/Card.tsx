faa3d569fc9437458373863f03a0b1da7094a9fb
choisohyun <sohyun4283@naver.com>
2020-08-13 15:23:09 +0900

    feat. 카드 재사용 컴포넌트 기본 스타일 추가
    [#13]

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 import Text from "@Style/Text";
 import Avatar from "@Style/Avatar";
@@ -19,5 +20,40 @@ const Card = () => {
     </>
   );
 };
+
+const Wrapper = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  border-radius: 0.5rem;
+  border: 1px solid ${({ theme }) => theme.mode.border};
+  box-shadow: 0 1px 3px ${({ theme }) => theme.mode.divider};
+  margin: 0 0.7rem;
+  padding: 1rem;
+  ${Text} {
+    width: 100%;
+  }
+`;
+
+const Title = styled(Text)`
+  height: 3rem;
+  white-space: normal;
+  word-wrap: break-word;
+  line-height: 1.2;
+  display: -webkit-box;
+  overflow: hidden;
+  margin-bottom: 1rem;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
+const AuthorWrapper = styled.div`
+  display: flex;
+  margin-top: 1rem;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Card;
