f84e2e01a0fd1c1d2baf2b5f3fa242565d42d7cd
choisohyun <sohyun4283@naver.com>
2020-08-13 15:12:56 +0900

    feat. 유저인지 여부를 props로 전달받아 다르게 표시되도록 추가
    [#14]

--- client/src/views/MainPage/Description/Description.tsx
+++ client/src/views/MainPage/Description/Description.tsx
@@ -4,21 +4,27 @@ import styled, { keyframes } from "styled-components";
 import Text from "@Style/Text";
 import Button from "@Style/Button";
 
-const Description = () => {
+interface IDescription {
+  isUser?: boolean;
+}
+
+const Description = ({ isUser }: IDescription) => {
   return (
     <>
       <Wrapper>
         <AnimWrapper />
-        <Phrase fontSize="xxl" fontWeight="extraBold">
-          지금 시작하여 퀴즈로
-          <br />
-          쉽게 공부해 보세요.
-        </Phrase>
+        {!isUser && (
+          <Phrase fontSize="xxl" fontWeight="extraBold">
+            지금 시작하여 퀴즈로
+            <br />
+            쉽게 공부해 보세요.
+          </Phrase>
+        )}
         <ButtonWrapper>
-          <Button>지금 바로 퀴즈 풀기</Button>
-          <Button>로그인하고 퀴즈 만들기</Button>
+          <Button>{isUser ? "문제 풀러 가기" : "지금 바로 퀴즈 풀기"}</Button>
+          <Button>{isUser ? "문제 만들러 가기" : "로그인하고 퀴즈 만들기"}</Button>
         </ButtonWrapper>
-        <Text fontWeight="semiBold">로그인 후 퀴즈를 풀면 풀이 기록을 남길 수 있습니다. :)</Text>
+        <Text fontWeight="semiBold">{!isUser && "로그인 후 퀴즈를 풀면 풀이 기록을 남길 수 있습니다. :)"}</Text>
       </Wrapper>
     </>
   );
