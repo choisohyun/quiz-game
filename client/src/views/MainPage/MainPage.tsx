52c227ce91d1b73ae146e4d4be0b28b3ebf83d5f
choisohyun <sohyun4283@naver.com>
2020-08-13 15:25:08 +0900

    feat. 메인 페이지에 베스트 퀴즈 목록 컴포넌트 추가
    [#13]

--- client/src/views/MainPage/MainPage.tsx
+++ client/src/views/MainPage/MainPage.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "@Header/Header";
 import Description from "@MainPage/Description/Description";
+import BestQuiz from "@MainPage/BestQuiz/BestQuiz";
 
 const MainPage = () => {
   return (
     <>
       <Header type="user" />
       <Description isUser={false} />
+      <BestQuiz />
     </>
   );
 };
