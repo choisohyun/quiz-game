9cdfe4de70f5730be20b7d188c101ff64c1f6536
choisohyun <sohyun4283@naver.com>
2020-08-17 12:11:45 +0900

    feat. 푸터 컴포넌트 작성
    [#12]

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
 const Footer = () => {
-  return <div></div>;
+  const gitUrl = "https://github.com/choisohyun";
+
+  return (
+    <>
+      <Wrapper>
+        Copyright © <a href={gitUrl}>choisohyun</a> All rights reserved.
+      </Wrapper>
+    </>
+  );
 };
+
+const Wrapper = styled.footer`
+  width: 100%;
+  border-top: 1px solid ${({ theme }) => theme.mode.gray3};
+  color: ${({ theme }) => theme.mode.primaryText};
+  font-size: 12px;
+  text-align: center;
+  line-height: 50px;
+  a:link,
+  a:visited,
+  a:active {
+    text-decoration: none;
+    color: ${({ theme }) => theme.mode.secondaryText};
+  }
+  a:hover {
+    text-decoration: none;
+    color: ${({ theme }) => theme.mode.background};
+  }
+`;
 
 export default Footer;
